5fc1437cfd0d6fa3e421f759f9a8e1fa9a21ba00
Radifan Mardhi Prana <43719706+radifanmp@users.noreply.github.com>
2020-08-04 09:49:02 +0700

    Class Component

---
+++ src/Components/Content.jsx
@@ -0,0 +1,12 @@
+import React,{Component} from 'react';
+import '../App.css';
+
+export default class Content extends Component {
+    render(){
+      return(
+        <div className="App-header">
+          <p>nah ini nama nya content paham klean, Batch 17</p>
+        </div>
+      )
+    }
+  }
---
+++ src/Components/Header.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+export default class Header extends Component {
+    render(){
+      return(
+        <div>
+          <h1>Ini headers Guys</h1>
+        </div>
+      )
+    }
+  }
