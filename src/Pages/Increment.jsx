08fc5185c7f8ba5f9ff6f6042f2f194b2d96ab74
Radifan Mardhi Prana <43719706+radifanmp@users.noreply.github.com>
2020-08-04 11:58:48 +0700

    Func State

--- src/Pages/Increment.jsx
+++ src/Pages/Increment.jsx
@@ -1,30 +1,47 @@
-import React,{Component} from 'react';
+import React, {Component, useState} from 'react';
 import '../App.css'
 import Header from '../Components/Header';
 
-class Increment extends Component{
-    constructor(){
-        super()
-        this.state = {
-            counter : 100
-        }
-    }
+// class Increment extends Component{
+//     constructor(){
+//         super()
+//         this.state = {
+//             counter : 100
+//         }
+//     }
 
-    render() {
-        return (
-            <div className="App">
-                <p>Click th button to increment ! !</p>
+//     render() {
+//         return (
+//             <div className="App">
+//                 <p>Click th button to increment ! !</p>
 
+//                 &nbsp;&nbsp;&nbsp;
+
+//                 <Header propertiku={this.state.counter}/>
+
+//                 &nbsp;&nbsp;&nbsp;
+
+//                 <button onClick={()=> this.setState({counter: this.state.counter + 100})} >Hit Me</button>
+//             </div>
+//         );
+//     }
+// }
+
+function Increment(){
+    const [counter, setCounter] = useState(100)
+
+    return(
+        <div className="App">
+            <p>Click th button to increment ! !</p>
                 &nbsp;&nbsp;&nbsp;
 
-                <Header propertiku={this.state.counter}/>
+            <Header propertiku={counter}/>
 
-                &nbsp;&nbsp;&nbsp;
+                 &nbsp;&nbsp;&nbsp;
 
-                <button onClick={()=> this.setState({counter: this.state.counter + 100})} >Hit Me</button>
-            </div>
-        );
-    }
+            <button onClick={()=> setCounter(counter+100)}> Hit Me</ button>
+        </div>
+    )
 }
 
 export default Increment
