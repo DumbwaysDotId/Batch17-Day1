6198a1e9686e70c1aa91971a069ba5f6f5e1e43f
Radifan Mardhi Prana <43719706+radifanmp@users.noreply.github.com>
2020-08-06 12:17:02 +0700

    fix css

--- src/Pages/Details.jsx
+++ src/Pages/Details.jsx
@@ -1,10 +1,31 @@
 import React from "react";
+import "./style.css";
 
 const Details = ({ match }) => {
   const { id } = match.params;
   return (
     <div>
       <h1>ini adalah details id: {id}</h1>
+      <h1>Flex</h1>
+      <h1>ini adalah details id: {id}</h1>
+      <h1>Flex</h1>
+      <h1>ini adalah details id: {id}</h1>
+      <h1>Flex</h1>
+      <h1>ini adalah details id: {id}</h1>
+      <h1>Flex</h1>
+      <h1>ini adalah details id: {id}</h1>
+      <h1>Flex</h1>
+      <h1>ini adalah details id: {id}</h1>
+      <h1>Flex</h1>
+      <h1>ini adalah details id: {id}</h1>
+      <h1>Flex</h1>
+      <h1>ini adalah details id: {id}</h1>
+      <h1>Flex</h1>
+      <h1>ini adalah details id: {id}</h1>
+      <h1>Flex</h1>
+      <div className="footer">
+        <div className="box" />
+      </div>
     </div>
   );
 };
